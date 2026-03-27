4f05a25053712879f38f2a986ec3d2a23cb9dc30
sayan <subhosen16@gmail.com>
2026-03-27 15:00:16 +0530

    Implemented Pagination capability

--- src/main/java/com/cs/springdatajpatutorial/controllers/ProductController.java
+++ src/main/java/com/cs/springdatajpatutorial/controllers/ProductController.java
@@ -2,6 +2,8 @@ package com.cs.springdatajpatutorial.controllers;
 
 import com.cs.springdatajpatutorial.entities.Product;
 import com.cs.springdatajpatutorial.repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +13,8 @@ import java.util.List;
 
 @RestController
 public class ProductController {
-    private final ProductRepository productRepository;;
+    private final ProductRepository productRepository;
+    private final Integer pageSize=5;
 
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -23,5 +26,12 @@ public class ProductController {
 //        return productRepository.findBy(Sort.by(Sort.Direction.DESC,sortBy));
         return productRepository.findBy(Sort.by(Sort.Direction.DESC,sortBy,"title","quantity"));
 //        return productRepository.findBy(Sort.by(sortBy).ascending());
+    }
+
+    @GetMapping(path = "/products/page")
+    public List<Product> findByPage(@RequestParam(name = "pageNumber", defaultValue = "0") Integer pageNumber,
+                                    @RequestParam(name = "sortBy", required = false, defaultValue = "id") String sortBy){
+//        return productRepository.findAll(PageRequest.of(pageNumber,pageSize));
+        return productRepository.findAll(PageRequest.of(pageNumber,pageSize, Sort.by(sortBy))).getContent();
     }
 }
